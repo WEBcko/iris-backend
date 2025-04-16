8deae898d9b19926413a53c718cb3aa88aafe7f9
Erick Ojeda <erickojeda2003@hotmail.com>
2025-04-16 19:39:31 -0300

    Teste

--- services/post_service.py
+++ services/post_service.py
@@ -61,6 +61,7 @@ class PostService:
         
         return [
             {
+                "teste" : "teste",
                 "id": post.id,
                 "title": post.title,
                 "description": post.description,

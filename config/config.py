6c36559f60cb45be552faae2634d8c56deb382ab
Erick Ojeda <erickojeda2003@hotmail.com>
2025-06-04 20:28:26 -0300

    Changes

--- config/config.py
+++ config/config.py
@@ -6,13 +6,8 @@ from datetime import timedelta
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "chave-secreta")
-    POSTGRES_USER = os.getenv("POSTGRES_USER", "posgres")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-    POSTGRES_DB = os.getenv("POSTGRES_DB", "blogdb")
-    
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}';
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@postgres:5432/blogd")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secreto")

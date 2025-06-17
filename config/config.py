17127e1a0353f972a09e5573d80516803b53dea9
Roberto Vinicius Passos <31169609+RvPassos@users.noreply.github.com>
2025-06-17 06:26:24 -0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@postgres:5432/blogd")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@postgres:5432/blogdb")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secreto")
